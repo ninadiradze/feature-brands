ab23f2040a511d7bcfd59b96c74e17a7cce5d091
mentesh <shota@mentesh.dev>
2021-10-30 17:36:16 +0400

    feat: [cherry-picked-rebased] header-footer

---
+++ packages/theme/components/Base/FooterNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FooterNavigation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ packages/theme/components/FooterFeatures.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FooterFeatures"
+}
+</script>
+
+<style scoped>
+
+</style>
